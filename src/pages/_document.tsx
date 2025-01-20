620f9ceb7bb94bdf69b74d41b32e65536ab3411e
고등어 <163974510+9bfish8@users.noreply.github.com>
2025-01-20 16:05:24 +0900

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,5 @@
-import Document, { Html, Head, Main, NextScript } from "next/document"
-import { CONFIG } from "site.config"
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import { CONFIG } from "site.config";
 
 class MyDocument extends Document {
   render() {
@@ -11,30 +11,32 @@ class MyDocument extends Document {
             rel="apple-touch-icon"
             sizes="192x192"
             href="/apple-touch-icon.png"
-          ></link>
+          />
           <link
             rel="alternate"
             type="application/rss+xml"
             title="RSS 2.0"
             href="/feed"
-          ></link>
+          />
+          {/* OG 이미지 설정 */}
+          <meta property="og:image" content="/og-image.png" />
+          <meta name="twitter:image" content="/og-image.png" />
+          <meta property="og:image:type" content="image/png" />
+          <meta property="og:image:width" content="1200" />
+          <meta property="og:image:height" content="630" />
           {/* google search console */}
           {CONFIG.googleSearchConsole.enable === true && (
-            <>
-              <meta
-                name="google-site-verification"
-                content={CONFIG.googleSearchConsole.config.siteVerification}
-              />
-            </>
+            <meta
+              name="google-site-verification"
+              content={CONFIG.googleSearchConsole.config.siteVerification}
+            />
           )}
           {/* naver search advisor */}
           {CONFIG.naverSearchAdvisor.enable === true && (
-            <>
-              <meta
-                name="naver-site-verification"
-                content={CONFIG.naverSearchAdvisor.config.siteVerification}
-              />
-            </>
+            <meta
+              name="naver-site-verification"
+              content={CONFIG.naverSearchAdvisor.config.siteVerification}
+            />
           )}
         </Head>
         <body>
@@ -42,8 +44,8 @@ class MyDocument extends Document {
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
 
-export default MyDocument
+export default MyDocument;
